439f09243a7ffa59ebe42df763dd9dbb0540065f
Ram Lakhan <ramlakhan.mobiledev@gmail.com>
2026-01-27 17:31:19 +0530

    initial commit

---
+++ src/test/java/com/rl/blogmanagementsystem/BlogmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rl.blogmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogmanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
